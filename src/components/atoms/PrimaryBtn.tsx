f7a70774fc413eae3f1789c70e24a4a6e26f7f10
shu915 <chukichi915@gmail.com>
2024-12-27 10:43:49 +0900

    ヘッダーをレイアウト化、ボタンをコンポーネント化

---
+++ src/components/atoms/PrimaryBtn.tsx
@@ -0,0 +1,7 @@
+
+
+export const PrimaryBtn = ({children}: {children: React.ReactNode}) => {
+  return (
+    <button className="bg-lime-800 text-white p-4 text-lg rounded-lg">{children}</button>
+  )
+}

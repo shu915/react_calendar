8b2d3e0e5adb86ecb3531f9d10887d9c7fed94ce
shu915 <chukichi915@gmail.com>
2025-01-01 16:01:40 +0900

    date-fnsを導入し、1カレンダーを作った

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,14 +1,53 @@
+import { eachWeekOfInterval, eachDayOfInterval, endOfMonth, endOfWeek, getMonth, startOfMonth, format, getDate } from "date-fns";
+import { DAYS_LIST } from "../../constants/calender";
+import { useState, useEffect, useMemo } from "react";
 
-
-import { useRecoilValue } from "recoil";
-import { loginUserState } from "../../store/loginUserState";
 
 export const CalendarPage = () => {
-  const loginUser = useRecoilValue(loginUserState); 
+  const today = useMemo(() => new Date(), []);
+  const [dateList, setDateList] = useState<Date[][]>([]);
+
+  useEffect(() => {
+    const sundayListOfMonth = eachWeekOfInterval({
+      start: startOfMonth(today),
+      end: endOfMonth(today),
+    })
+    const newDateList = sundayListOfMonth.map((date) => {
+      return eachDayOfInterval({
+        start: date,
+        end: endOfWeek(date),
+      })
+    })
+    setDateList(newDateList);
+    console.log(newDateList);
+  }, [today]);
   return (
     <>
-    <p>{loginUser.id}</p>
-    <p>{loginUser.name}</p>
+      <h1 className="font-bold text-3xl mb-5">{getMonth(today) + 1}月</h1>
+      <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
+        <thead>
+          <tr className="bg-lime-800 text-white rounded-tl-lg rounded-tr-lg py-10">
+            {DAYS_LIST.map((day) => (
+              <th className="text-center text-xl py-3" key={day}>{day}</th>
+            ))}
+          </tr>
+
+        </thead>
+        <tbody>
+          {dateList.map((week) => (
+            <tr key={`week-${getDate(week[0])}`} className="mx-10">
+              {week.map((date) => (
+                <td key={`date-${getDate(date)}`} className=" bg-white h-[10vh] border-2 border-solid border-lime-800 align-top">
+                  <span className="inline-block w-[20px]">
+                    {getDate(date)}
+                  </span>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
     </>
   )
 }

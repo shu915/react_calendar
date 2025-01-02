59a9d696db54c0ada852c282fcaee01ba1f6dfc5
shu915 <chukichi915@gmail.com>
2025-01-02 12:10:49 +0900

    カレンダーのロジックをuseCalendarに切り出した

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,49 +1,11 @@
-import { eachWeekOfInterval, eachDayOfInterval, endOfMonth, endOfWeek, getMonth, startOfMonth, isSameDay } from "date-fns";
-import { useState, useEffect, useMemo } from "react";
+import { getMonth } from "date-fns";
+import { useCalender } from "../../hooks/useCalender";
 import { CalenderHeader } from "../organisms/CalenderHeader";
 import { CalenderBody } from "../organisms/CalenderBody";
-import { DateList, Schedule } from "../../types/calendar";
-import { getScheduleList } from "../../api/calendar";
 
 export const CalendarPage = () => {
-  const today = useMemo(() => new Date(), []);
-  const [dateList, setDateList] = useState<DateList>([]);
-
-  
-
-  useEffect(() => {
-    const sundayListOfMonth = eachWeekOfInterval({
-      start: startOfMonth(today),
-      end: endOfMonth(today),
-    })
-    const newDateList: DateList = sundayListOfMonth.map((date) => {
-      return eachDayOfInterval({
-        start: date,
-        end: endOfWeek(date),
-      }).map((date) => ({date, schedules: [] as Schedule[]}))
-      })
-      
-    const scheduleList = getScheduleList();
-      
-      scheduleList.forEach((schedule) => {
-        const firstIndex = newDateList.findIndex(oneWeek => {
-          return oneWeek.some(item => isSameDay(item.date, schedule.date));
-        })
-        console.log(firstIndex);
-        if (firstIndex === -1) {
-          return;
-        }
-        const secondIndex = newDateList[firstIndex].findIndex(item => {
-          return isSameDay(item.date, schedule.date);
-        })
-        if (secondIndex === -1) {
-          return;
-        }
-        newDateList[firstIndex][secondIndex].schedules = [...newDateList[firstIndex][secondIndex].schedules, schedule];
-      })
-      setDateList(newDateList);
-    console.log(newDateList);
-  }, [today]);
+  const today = new Date();
+  const {dateList} = useCalender({currentDate: today});
   
   return (
     <>

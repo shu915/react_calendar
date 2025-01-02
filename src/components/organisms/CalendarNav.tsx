3b70a31c2d14d4fa5cf12c2b62587d8f603a50aa
shu915 <chukichi915@gmail.com>
2025-01-02 12:56:39 +0900

    カレンダーのナビゲーションで、先月、次月に飛べるようになった

--- src/components/organisms/CalendarNav.tsx
+++ src/components/organisms/CalendarNav.tsx
@@ -1,13 +1,24 @@
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa"
 import { PrimaryBtn } from "../atoms/PrimaryBtn"
+import { Dispatch, SetStateAction } from "react";
+import { addMonths, subMonths } from "date-fns";
 
-export const CalendarNav = () => {
+type Props = {
+  setCurrentDate: Dispatch<SetStateAction<Date>>;
+}
+
+export const CalendarNav = ({setCurrentDate }: Props) => {
+  const changeToday = () => setCurrentDate(new Date());
+  const changePrevMonth = () => setCurrentDate(prevDate => subMonths(prevDate, 1));
+  const changeNextMonth = () => setCurrentDate(prevDate => addMonths(prevDate, 1));
+
+
   return (
     <div className="w-[80%] flex justify-between mb-2">
       <div className="flex items-center text-white gap-4">
-        <FaArrowAltCircleLeft className="text-lime-800 text-2xl" />
-        <PrimaryBtn size="sm" onClick={() => null}>今月</PrimaryBtn>
-        <FaArrowAltCircleRight className="text-lime-800 text-2xl" />
+        <FaArrowAltCircleLeft className="text-lime-800 text-2xl" onClick={changePrevMonth} />
+        <PrimaryBtn size="sm" onClick={changeToday}>今月</PrimaryBtn>
+        <FaArrowAltCircleRight className="text-lime-800 text-2xl" onClick={changeNextMonth} />
       </div>
     </div>
   )
